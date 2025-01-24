0ca8a495958d82e70e486c20282ab30225ce08c9
Pavithra17-qwe <pavithra@qaoncloud.com>
2025-01-24 22:39:43 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'j4sqoe',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
